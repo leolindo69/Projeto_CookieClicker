b56afb3a55e4ca5aad2fbde9ea7e1f5fdad219ae
leolindo69 <ribeiroleonardoantonio@gmail.com>
2025-03-31 08:42:05 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+import React,{userstate} from 'react'
+import {view,text,touchaleOpacity,styleSheet} from
+
+export default function App(){
+//CRIANDO A VARIAVEL PARA ARMAZENAR A QUANTIDADE DE CLICKS
+const[Pontos,Setpontos] = () => {
+Setpontos(pontos + 1);
+}
+//PARTE VISUAL DO APLICATIVO 
+Return(
+<view style={styles.container}>
+//exibir a quantidade de cookies na tela 
+<text style=(styles.text)cookies:{cookies}</text>
+  {//botão para clicar e aumentar os cookies//}
+  <TouchableOpacity style={styles.button} onPress={handleClick}>
+    <Text style={styles.buttonText}>Clique para ganhar cookies!</text>
+    </TouchableOpacity> 
+    </View>
+    );
+  }
+//estilos para os componentes de interfaces
+const style =StyleSheet.create({
+  container: {
+    flex:1
+    justifyContent: 'center',
+  alignItens:'center'
+  backgroundColor:'#f8f8f8',
+    },
+text:{
+  front:24
+  margiBottom:20,
+    },
+buttonText:{
+  fronteSize:18
+  
